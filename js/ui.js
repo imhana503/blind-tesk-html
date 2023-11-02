7f3b725fd1d189d4039e410c973599583b4fae92
LHN <imhana503@pulip.com>
2023-11-02 17:52:23 +0900

    .

--- js/ui.js
+++ js/ui.js
@@ -1,15 +1,56 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM fully loaded and parsed");
+  //전체메뉴
+  const allMenuAction = () => {
+    const btnAllMenuOpen = document.querySelector('.btn-allmenu');
+    const btnAllMenuClose = document.querySelector('.all-menu .btn-close');
 
-    const allMenu = () => {
-      const btnAllMenu = document.querySelector('.all-menu');
-      console.log(btnAllMenu);
-      btnAllMenu.addEventListener('click', function(){
-        console.log('aa')
+
+    //전체메뉴 열기
+    btnAllMenuOpen.addEventListener("click", function(e) {
+      const allMenu = document.querySelector('.all-menu');
+     
+      this.setAttribute('aria-expanded', true);
+      allMenu.classList.add('is-active');
+      allMenu.setAttribute('aria-hidden', false);
+    });
+
+    //전체메뉴 닫기
+    btnAllMenuClose.addEventListener("click", function() {
+      const allMenu = this.closest('.all-menu');
+
+      this.setAttribute('aria-expanded', false);
+      allMenu.classList.remove('is-active');
+      allMenu.setAttribute('aria-hidden', true);
+    });
+  }
+
+  const layerPopup = () => {
+    const btnLayerOpen = document.querySelectorAll('.openLayerPoup');
+
+    btnLayerOpen.forEach(function(item){
+      item.addEventListener('click', function(){
+        
+        console.log(this)
       });
-    }
+    })
+  }
 
 
-    allMenu(); //전체메뉴
-    
+  //탭메뉴
+  const tabAction = () => {
+    const tabs = document.querySelectorAll('.tab');
+
+    tabs.forEach(function(item){
+      const array = new Array(item)
+      console.log(array)
+      array
+      
+    })
+  }
+
+  allMenuAction(); //전체메뉴
+  layerPopup();//팝업
+  tabAction();//탭메뉴
+  
+
 });
