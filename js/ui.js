e876ecaed92be754054c0565bd79936e81d0377e
unknown <imhana503@gmail.com>
2023-11-08 06:46:06 +0900

    .

--- js/ui.js
+++ js/ui.js
@@ -38,14 +38,68 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   //탭메뉴
   const tabAction = () => {
-    const tabs = document.querySelectorAll('.tab');
+    const dataTabs = document.querySelectorAll('[data-element="tab"]');
 
-    tabs.forEach(function(item){
-      const array = new Array(item)
-      console.log(array)
-      array
-      
+    dataTabs.forEach(function(dataTab){
+      const btnTabs = dataTab.querySelectorAll('.tab-list>li>a');
+
+      btnTabs.forEach(function(btnTab){
+        btnTab.addEventListener('click', function(e){
+          const target = e.target;
+          const tabPanel = target.closest('.tab').querySelectorAll('.tab-panel');
+
+          target.closest('.tab-list').querySelectorAll('li').forEach(function(li){
+            li.classList.remove('is-active');
+            li.querySelector('a').setAttribute('aria-selected', 'false');
+            li.querySelector('a').setAttribute('tabindex', -1)
+          })
+          
+          target.setAttribute('aria-selected', 'true');
+          target.closest('li').classList.add('is-active');
+          li.querySelector('a').setAttribute('tabindex', 0)
+
+          const nodes = [...e.target.closest('.tab-list').querySelectorAll('li')];
+          const idx = nodes.findIndex(node=>node.classList.contains('is-active'));
+
+          tabPanel.forEach(function(panel){
+            panel.classList.remove('is-active');
+          })
+          tabPanel[idx].classList.add('is-active');
+        });
+
+
+        //탭 포커스 이동
+        let keyIdx = 0;
+        btnTab.addEventListener('keydown', function(e){
+          console.log(e.keyCode);
+
+          if( e.keyCode === 39 ){
+            const target = e.target;
+            const nodes = [...e.target.closest('.tab-list').querySelectorAll('li')];
+            nodes[keyIdx].classList.add('is-active');
+            nodes[keyIdx].querySelector('a').setAttribute('tabindex', 0);
+            nodes[keyIdx].querySelector('a').focus();
+            keyIdx = keyIdx+=1;
+
+            return;
+          } 
+
+          if( e.keyCode === 37 ){
+            const target = e.target;
+            const nodes = [...e.target.closest('.tab-list').querySelectorAll('li')];
+            nodes[keyIdx].classList.add('is-active');
+            nodes[keyIdx].querySelector('a').focus();
+            keyIdx = keyIdx-=1;
+
+            return;
+          } 
+        })
+      })
+
+     
     })
+
+
   }
 
   const fixedBottom = () => {
@@ -62,10 +116,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   //allMenuAction(); //전체메뉴
   // layerPopup();//팝업
-  // tabAction();//탭메뉴
-
-
-  fixedBottom(); //하단 고정 영역시 높이 값 설정
+  tabAction();//탭메뉴
+  //fixedBottom(); //하단 고정 영역시 높이 값 설정
   
 
 });
