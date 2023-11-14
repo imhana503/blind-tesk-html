fcd008b5bc9252cbb89a0a9e433431f849b2b521
LHN <imhana503@pulip.com>
2023-11-14 18:10:27 +0900

    .

---
+++ front/guide/ia/data-page.js
@@ -0,0 +1,73 @@
+const dataHtmlList = [
+    {
+       
+        menuName:'인트로 - 첫화면 진입(비회원)',
+        menuNames:'intro',
+        menuUrl:'./../../html/intro.html',
+        menuState:'완료'
+    },
+    {
+       
+        menuName:'인트로 - 첫화면 회원가입 방법 선택',
+        menuNames:'intro-member',
+        menuUrl:'./../../html/intro-member.html',
+        menuState:'완료'
+    },
+    {
+       
+        menuName:'인트로 - 초기화면진입',
+        menuNames:'intro-join',
+        menuUrl:'./../../html/intro-join.html',
+        menuState:'완료  '
+    },
+    {
+       
+        menuName:'상품목록', 
+        menuNames:'product',
+        menuUrl:'./../../html/product.html',
+        menuState:'완료'
+    },
+    {
+       
+        menuName:'상품상세',
+        menuNames:'prodect-detail',
+        menuUrl:'./../../html/prodect-detail.html',
+        menuState:'완료'
+    },
+    {
+       
+        menuName:'팝업(바텀) 상품상세',
+        menuNames:'popup-bottom-optionSelect',
+        menuUrl:'./../../html/popup-bottom-optionSelect',
+        menuState:'팝업 접근성 수정'
+    },
+    {
+       
+        menuName:'팝업(바텀) 옵션선택',
+        menuNames:'popup-bottom-optionSelect',
+        menuUrl:'./../../html/popup-bottom-optionSelect.html',
+        menuState:'팝업 접근성 수정'
+    },
+    {
+       
+        menuName:'주문결제',
+        menuNames:'payment',
+        menuUrl:'./../../html/payment.html',
+        menuState:'완료'
+    },
+    {
+       
+        menuName:'완료 페이지',
+        menuNames:'complete',
+        menuUrl:'./../../html/complete.html',
+        menuState:'완료'
+    },
+    {
+       
+        menuName:'팝업(풀) 검색하기',
+        menuNames:'popup-full-search',
+        menuUrl:'popup-full-search.html',
+        menuState:'팝업 접근성 수정'
+    },
+];
+
